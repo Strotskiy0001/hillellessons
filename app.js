d874b13f3aad1f6996b6a47f4749615b131ab1ce
Vadim <strotskiy.vv@gmail.com>
2022-04-28 21:58:12 +0300

    hw9

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
-const textE = document.getElementById("text");
-const btnE = document.querySelector("#btn");
-const ulContainerE = document.getElementById("list");
-const inptTextE = document.getElementById("inpt");
+const nameE = document.getElementById("first-name");
+const lastE = document.getElementById("second-name");
+const phoneE = document.getElementById("phone");
+const containerE = document.querySelector(".container");
+const template = document.getElementById("template").innerHTML;
+const btnE = document.getElementById("btn");
 
-btnE.addEventListener("click", onClick);
+btnE.addEventListener("click", onAddTodo);
 
-function onClick() {
-  const liE = document.createElement("li");
-  liE.textContent = inptTextE.value;
-  ulContainerE.append(liE);
-  inptTextE.value = "";
+function onAddTodo() {
+  const name = nameE.value;
+  const last = lastE.value;
+  const phone = phoneE.value;
+  if (!isDataValid(name, last, phone)) {
+    alert("Error, please try again");
+    clearData();
+    return;
+  }
+  const el = createContactElement(name, last, phone, template);
+  renderElement(containerE, el);
+  clearData();
+}
+
+function isDataValid(name, last, phone) {
+  if (!name.trim() || !last.trim() || !phone.trim() || isNaN(+phone)) {
+    return false;
+  }
+  return true;
+}
+
+function clearData() {
+  nameE.value = "";
+  lastE.value = "";
+  phoneE.value = "";
+}
+
+function createContactElement(name, last, phone, template) {
+  return template
+    .replace("{{name}}", name)
+    .replace("{{last}}", last)
+    .replace("{{phone}}", phone);
+}
+
+function renderElement(container, element) {
+  container.innerHTML += element;
 }
