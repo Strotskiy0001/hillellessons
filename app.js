c8ae15c2b311ef9ac8dccfda32b0ebb09da9f908
Vadim <strotskiy.vv@gmail.com>
2022-05-13 01:33:49 +0300

    hw12

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
-const arr = [1, 2, 3, 4, 5, 6, 7];
-let result = 0;
+const sizes = {
+  small: {
+    price: 50,
+    calories: 20,
+  },
+  medium: {
+    price: 75,
+    calories: 30,
+  },
+  big: {
+    price: 100,
+    calories: 40,
+  },
+};
 
-function rec(array, index) {
-  if (array[index]) {
-    result += array[index];
-    rec(arr, --index);
-  }
-  return result;
+const topings = {
+  cheese: {
+    price: 10,
+    calories: 20,
+  },
+  salad: {
+    price: 20,
+    calories: 5,
+  },
+  potatoes: {
+    price: 15,
+    calories: 10,
+  },
+  seasoning: {
+    price: 15,
+    calories: 0,
+  },
+  mayonnaise: {
+    price: 20,
+    calories: 5,
+  },
+};
+
+function selectHamburger(sizes) {
+  this.sizes = sizes;
+  this.topings = [];
 }
 
-rec(arr, 4);
-console.log(result);
+selectHamburger.prototype.addTopping = function (topings) {
+  this.topings.push(topings);
+};
+
+selectHamburger.prototype.getPrice = function () {
+  return this.topings.reduce((a, e) => (a += e.price), this.sizes.price);
+};
+
+selectHamburger.prototype.getCalories = function () {
+  return this.topings.reduce((a, e) => (a += e.calories), this.sizes.calories);
+};
+
+const mediumHamburger = new selectHamburger(sizes.medium);
+mediumHamburger.addTopping(topings.cheese);
+mediumHamburger.addTopping(topings.mayonnaise);
+
+const price = mediumHamburger.getPrice();
+console.log(`Your total price of hamburger - ${price} UAH`);
+const caloriesAll = mediumHamburger.getCalories();
+console.log(`Your total calories of hamburger - ${caloriesAll}`);
