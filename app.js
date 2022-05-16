7ef4892d3416950d4a2a48abdcd8a63d3467e937
Vadim <strotskiy.vv@gmail.com>
2022-05-16 22:02:36 +0300

    hw13

--- app.js
+++ app.js
@@ -1,63 +1,33 @@
-const sizes = {
-  small: {
-    price: 50,
-    calories: 20,
-  },
-  medium: {
-    price: 75,
-    calories: 30,
-  },
-  big: {
-    price: 100,
-    calories: 40,
-  },
-};
+class Group {
+  #arrStudents = [];
+  allMarks = [];
+  avgMarks = 0;
 
-const topings = {
-  cheese: {
-    price: 10,
-    calories: 20,
-  },
-  salad: {
-    price: 20,
-    calories: 5,
-  },
-  potatoes: {
-    price: 15,
-    calories: 10,
-  },
-  seasoning: {
-    price: 15,
-    calories: 0,
-  },
-  mayonnaise: {
-    price: 20,
-    calories: 5,
-  },
-};
-
-function selectHamburger(sizes) {
-  this.sizes = sizes;
-  this.topings = [];
+  addStudent({ fullName, marks }) {
+    this.#arrStudents.push({ fullName, marks });
+  }
+  getAverageMark({ marks }) {
+    this.allMarks.push(...marks);
+    this.avgMarks =
+      this.allMarks.reduce((a, b) => a + b, 0) / this.allMarks.length;
+  }
 }
 
-selectHamburger.prototype.addTopping = function (topings) {
-  this.topings.push(topings);
-};
+class Student {
+  constructor(fullName, marks) {
+    this.fullName = fullName;
+    this.marks = marks;
+  }
+}
 
-selectHamburger.prototype.getPrice = function () {
-  return this.topings.reduce((a, e) => (a += e.price), this.sizes.price);
-};
-
-selectHamburger.prototype.getCalories = function () {
-  return this.topings.reduce((a, e) => (a += e.calories), this.sizes.calories);
-};
-
-const mediumHamburger = new selectHamburger(sizes.medium);
-mediumHamburger.addTopping(topings.cheese);
-mediumHamburger.addTopping(topings.mayonnaise);
-
-const price = mediumHamburger.getPrice();
-console.log(`Your total price of hamburger - ${price} UAH`);
-const caloriesAll = mediumHamburger.getCalories();
-console.log(`Your total calories of hamburger - ${caloriesAll}`);
+const firstStudent = new Student("John Doe", [10, 102, 0]);
+const secondStudent = new Student("Alex Smith", [10, 9, 8]);
+const thirdStudent = new Student("Bob Johnson", [9, 10, 10, 8]);
+const feGroup = new Group();
+feGroup.addStudent(firstStudent);
+feGroup.addStudent(secondStudent);
+feGroup.addStudent(thirdStudent);
+feGroup.getAverageMark(firstStudent);
+feGroup.getAverageMark(secondStudent);
+feGroup.getAverageMark(thirdStudent);
+console.log(feGroup.avgMarks);
