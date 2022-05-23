c14622c61c0a4151b2d20e3e060e39eaa2d23ab0
Vadim <strotskiy.vv@gmail.com>
2022-05-23 20:34:51 +0300

    hw

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-const tabs = new Tab(document.querySelector(".tab-container"));
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
