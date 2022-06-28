de26cd9c520730ce2ee33b8520196b4836d0fb9a
Vadim <strotskiy.vv@gmail.com>
2022-06-28 21:19:37 +0300

    hw20111

---
+++ src/calculator/index.js
@@ -0,0 +1,11 @@
+const add = require("./add/add");
+const min = require("./min/min");
+const mul = require("./mul/mul");
+const div = require("./div/div");
+
+module.exports = {
+  add: add.add,
+  min: min.min,
+  mul: mul.mul,
+  div: div.div,
+};
---
+++ src/index.js
@@ -0,0 +1,4 @@
+const calculator = require("./calculator");
+
+console.log(calculator.add(3, 4));
+console.log(calculator.div(10, 2));
